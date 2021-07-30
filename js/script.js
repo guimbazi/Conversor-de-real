06bce978e323e93dddfcef5423f39a8167f4f2a3
guimbazi <danieluizf.defreitas@yahoo.com.br>
2021-07-30 19:28:00 -0300

    Upload
    Primeiro upload do conversor de real para outras moedas estrangeiras criado a partir de um projeto da semana "Mission One".
    Esta é uma versão modificada, e um pouco melhorada da versão original iniciada no projeto original.

---
+++ js/script.js
@@ -0,0 +1,54 @@
+// MAP
+let moeda = document.querySelector('#moeda-convertida');
+
+// EVENTOS
+document.querySelector('#botao').addEventListener('click', converter);
+document.querySelector('#moeda-convertida').addEventListener('change', selectMoeda);
+
+// FUNÇÕES
+async function converter() {
+
+    let moedas = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL").then(function(resposta){
+    return resposta.json()
+    });
+    let dolar = moedas.USDBRL.high
+    let euro = moedas.EURBRL.high
+
+
+    document.querySelector('#secundaria').style.display = 'block';
+    let inputValorEmReal = Number(document.querySelector('#input').value);
+
+    if (moeda.value === '') {
+        document.querySelector('#secundaria').style.display = 'none';
+    }
+    if (moeda.value === 'US$ Dólar americano') {
+        let valorDolares = inputValorEmReal / dolar;
+        document.querySelector('#input-moedas').innerHTML = valorDolares.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+    }
+    
+    if (moeda.value === '€ Euro') {
+        let valorEuro = inputValorEmReal / euro;
+        document.querySelector('#input-moedas').innerHTML = valorEuro.toLocaleString('de-DE', { style: 'currency', currency: 'EUR' });
+    }
+    document.querySelector('#input-real').innerHTML = inputValorEmReal.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
+    
+}
+
+function selectMoeda() {
+    if (moeda.value === '') {
+        document.querySelector('#secundaria').style.display = 'none';
+        document.querySelector('#input').value = '';
+    }
+    if (moeda.value === 'US$ Dólar americano') {
+        document.querySelector('#bandeira-convertida').src = '../img/eua.png';
+        document.querySelector('#moeda').innerHTML = 'Dólar Americano';
+        
+    }
+    if (moeda.value === '€ Euro') {
+        document.querySelector('#bandeira-convertida').src = '../img/euro.png';
+        document.querySelector('#moeda').innerHTML = 'Euro';
+        
+    }
+    document.querySelector('#input-moedas').innerHTML = '';
+}
+
